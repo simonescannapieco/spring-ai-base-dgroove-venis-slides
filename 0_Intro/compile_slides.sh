09ff1a33af9bc604610dffa9e1b249c613de57bd
Simone Scannapieco <simone.scannapieco@realt.it>
2025-08-27 11:52:26 +0200

    Standardizations

--- 0_Intro/compile_slides.sh
+++ 0_Intro/compile_slides.sh
@@ -5,5 +5,8 @@ rm -rf out/main*
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
 
-xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_notes.tex
-xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_notes.tex
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_handout.tex
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_handout.tex
+
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_handout_notes.tex
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_handout_notes.tex
