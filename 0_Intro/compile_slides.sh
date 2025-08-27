e3d4f2e17516f87ba6bfbc2ef95fa386775e3c81
Simone Scannapieco <simone.scannapieco@realt.it>
2025-08-27 10:44:07 +0200

    Standardizations

--- 0_Intro/compile_slides.sh
+++ 0_Intro/compile_slides.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf out/main.*
+rm -rf out/main*
 
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
