c44ef6bbf7c9a10bc8098a714a4fbe3f84f1e31e
Simone Scannapieco <simone.scannapieco@realt.it>
2025-08-27 10:36:50 +0200

    Standardizations

--- 2_NLP_Language_AI/compile_slides.sh
+++ 2_NLP_Language_AI/compile_slides.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 rm -rf out/main.*
+
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
+
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_notes.tex
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_notes.tex
