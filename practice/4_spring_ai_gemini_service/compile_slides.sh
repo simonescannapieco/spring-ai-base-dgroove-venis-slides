e8d84ef14d4f4047359594f204964763e95d5a16
Simone Scannapieco <simone.scannapieco@realt.it>
2025-09-16 13:35:17 +0200

    Massive template modifications

--- practice/4_spring_ai_gemini_service/compile_slides.sh
+++ practice/4_spring_ai_gemini_service/compile_slides.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf out/main*
+rm -rf out/*
 
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main.tex
@@ -10,3 +10,7 @@ xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out 
 
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_handout_notes.tex
 xelatex -shell-escape -synctex=1 -interaction=nonstopmode -output-directory=out main_handout_notes.tex
+
+mv out/main.pdf out/${PWD##*/}.pdf
+mv out/main_handout.pdf out/${PWD##*/}_handout.pdf
+mv out/main_handout_notes.pdf out/${PWD##*/}_handout_notes.pdf
